9a8f22347daa7d0e7fd5a838a8bf3b4663858b54
LeapGenSolutions <website@leapgen.ai>
2025-12-01 20:59:09 -0600

    Updated StreamIO key

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL = "https://seismic-app-server-uat-gjdzdrewfmhscugq.westus-01.azurewebsites.net"
-export const GETSTREAM_APIKEY = "qb4swzqqxr76"
+export const GETSTREAM_APIKEY = "23dnbb2d3q8q"
